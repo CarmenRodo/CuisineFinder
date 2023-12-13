8a2b458998f6134773cd5ab9fa41098be0240164
rodocarm <rcarmen@my.wctc.edu>
2023-12-13 17:25:27 -0600

    changed dist to docs

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === "production" ? "/CuisineFinder/" : "/",
-    outputDir: "docs", // Set the output directory to "docs"
+    outputDir: "dist", // Set the output directory to "docs"
 };
